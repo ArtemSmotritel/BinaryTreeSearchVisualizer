9cccb741debd81c13e0782e0ad7106783913d683
Artem Smotrytel <artemsmotritel@gmail.com>
2022-12-07 13:04:58 +0200

    add Utils class

---
+++ BinaryTreeSearchVisualizer/src/Utils.cs
@@ -0,0 +1,15 @@
+﻿namespace BinaryTreeSearchVisualizer.src
+{
+    internal static class Utils
+    {
+        public static long ExtractNumber(string input)
+        {
+            var success = long.TryParse(input, out long result);
+            if (success)
+            {
+                return result;
+            }
+            throw new Exception("Entered value is not a number");
+        }
+    }
+}
