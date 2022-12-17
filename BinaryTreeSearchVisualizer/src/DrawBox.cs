fd20584e1f97d2df908aeeb6054a3d8e7105f79c
Artem Smotrytel <artemsmotritel@gmail.com>
2022-12-17 02:22:08 +0200

    create drawBox component

---
+++ BinaryTreeSearchVisualizer/src/DrawBox.cs
@@ -0,0 +1,28 @@
+﻿using BinaryTreeSearchVisualizer.src.ApplicationTree;
+using System.Drawing.Drawing2D;
+
+namespace BinaryTreeSearchVisualizer.src
+{
+    internal class DrawBox : PictureBox
+    {
+        public DrawBox() 
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        public void DrawTree(BinaryTree binaryTree, Graphics graphics)
+        {
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            var nodeInfos = binaryTree.GetNodesInfo();
+            foreach (var node in nodeInfos)
+            {
+                GraphicUtils.DrawNode(node, graphics);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+    }
+}
