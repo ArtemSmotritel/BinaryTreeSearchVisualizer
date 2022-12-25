8a3eee7db06d19aa8e4c7a046ec5c11d3cba63dc
Artem Smotrytel <artemsmotritel@gmail.com>
2022-12-25 14:03:42 +0200

    create VisualizerProperty class

---
+++ BinaryTreeSearchVisualizer/properties/VisualizerProperty.cs
@@ -0,0 +1,13 @@
+﻿namespace BinaryTreeSearchVisualizer.properties
+{
+    internal static class VisualizerProperty
+    {
+        public static Point rootCenter = new Point(2500, 50);
+        public static Size drawBoxSize = new Size(5000, 3000);
+
+        public static int timeGap = 600;
+
+        public static int nodeRadius = 30;
+        public static int nodeOffset = 60;
+    }
+}
