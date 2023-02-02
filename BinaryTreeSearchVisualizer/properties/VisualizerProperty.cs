3659c671a515eb7885d1c160023cc62eca45e737
Artem Smotrytel <artemsmotritel@gmail.com>
2023-02-02 02:45:57 +0200

    add traverse colors to Property class

--- BinaryTreeSearchVisualizer/properties/VisualizerProperty.cs
+++ BinaryTreeSearchVisualizer/properties/VisualizerProperty.cs
@@ -9,5 +9,16 @@
 
         public static int nodeRadius = 30;
         public static int nodeOffset = 60;
+
+        public static Color traverseColor = Color.Blue;
+
+        public static Color findInsertColor = Color.Green;
+        public static Color missingInsertColor = Color.Blue;
+
+        public static Color findRemoveColor = Color.Red;
+        public static Color missingRemoveColor = Color.Blue;
+
+        public static Color findFindColor = Color.Green;
+        public static Color missingFindColor = Color.Red;
     }
 }
